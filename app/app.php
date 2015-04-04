059ba528feaca4287c82cafeda8689ba6814a038
Kelly de Vries <kdv@me.com>
2015-04-03 19:32:04 -0700

    Set all initial get paths for stores, store, and store_edit.

--- app/app.php
+++ app/app.php
@@ -35,5 +35,28 @@
         return $app['twig']->render('/brand.twig');
     });
 
+    //displays brand to be updated or deleted
+    $app->get("/brand_edit", function () use ($app)
+    {
+        return $app['twig']->render('/brand_edit.twig');
+    });
+
+    //displays all stores
+    $app->get("/stores", function () use ($app)
+    {
+        return $app['twig']->render('/stores.twig');
+    });
+
+    //displays one store and all brands associated with that store
+    $app->get("/store", function () use ($app)
+    {
+        return $app['twig']->render('/store.twig');
+    });
+
+    //displays store to be updated or deleted
+    $app->get("/store_edit", function () use ($app)
+    {
+        return $app['twig']->render('/store_edit.twig');
+    });
     return $app;
 ?>
