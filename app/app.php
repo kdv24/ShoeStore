a4405d5d5866fb9ff1c1c4b0649a91b431ee5720
Kelly de Vries <kdv@me.com>
2015-04-04 07:11:53 -0700

    Add patch function to store and brand, with comment out for brand, since unneccesary for this assignment.

--- app/app.php
+++ app/app.php
@@ -15,7 +15,7 @@
     Request::enableHttpMethodParameterOverride();
 
 
-//READ
+//HOME
 
     //displays links to brands and stores pages
     $app->get("/", function () use ($app)
@@ -23,6 +23,7 @@
         return $app['twig']->render('/index.twig');
     });
 
+//BRANDS
     //displays all brands
     $app->get("/brands", function () use ($app)
     {
@@ -35,24 +36,33 @@
         return $app['twig']->render('/brands.twig');
     });
 
-    //displays one brand and all stores associated with that brand
-    $app->get("/brand", function () use ($app)
+    //displays one brand and all stores associated with that brand($id)
+    $app->get("/brand/{id}", function ($id) use ($app)
     {
         return $app['twig']->render('/brand.twig');
     });
 
     //receives all info from the form (add a store to the brand) on the brand page
-    $app->post("/brand", function () use ($app)
+    $app->post("/brand/{id}", function ($id) use ($app)
     {
         return $app['twig']->render('/brand.twig');
     });
 
-    //displays brand to be updated or deleted
-    $app->get("/brand_edit", function () use ($app)
-    {
-        return $app['twig']->render('/brand_edit.twig');
-    });
+//NOT NECESSARY FOR THIS ASSIGNMENT
+    //displays brand to be updated or deleted- maybe don't really need GET?  see note with store patch below
+    // $app->get("/brand/{id}/edit", function ($id) use ($app)
+    // {
+    //     return $app['twig']->render('/brand_edit.twig');
+    // });
 
+ 
+    //updates the specific brand by id from the brand{id}.
+    // $app->patch("/brand/{id}/edit", function ($id) use ($app)
+    // {
+    //     return $app['twig']->render('brand.twig');
+    // });
+
+//STORES
     //displays all stores
     $app->get("/stores", function () use ($app)
     {
@@ -66,22 +76,28 @@
     });
 
     //displays one store and all brands associated with that store
-    $app->get("/store", function () use ($app)
+    $app->get("/store/{id}", function ($id) use ($app)
     {
         return $app['twig']->render('/store.twig');
     });
 
     //receives all info from the form (add a brand to the store) on the store page
-    $app->post("/store", function () use ($app)
+    $app->post("/store/{id}", function ($id) use ($app)
     {
-        return $app['twig']->render('/store.twig');
+        return $app['twig']->render('store.twig');
     });
 
-
-    //displays store to be updated or deleted
-    $app->get("/store_edit", function () use ($app)
+    //displays store to be updated or deleted- maybe don't really need GET unless this is where form displays to enter changed info? could also put that on store page with path to /store{id}/edit and never display store_edit.twig?
+    $app->get("/store/{id}/edit", function ($id) use ($app)
     {
-        return $app['twig']->render('/store_edit.twig');
+        return $app['twig']->render('store_edit.twig');
     });
+
+    //updates the specific store name using id from store{id}
+    $app->patch("/store/{id}/edit", function ($id) use ($app)
+    {
+        return $app['twig']->render('store.twig');;
+    });
+
     return $app;
 ?>
