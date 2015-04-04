1ee321189aa0e01971d48c86b60f3673c1f60461
Kelly de Vries <kdv@me.com>
2015-04-04 07:36:31 -0700

    Add more commenting to routes for clarification.

--- app/app.php
+++ app/app.php
@@ -15,7 +15,7 @@
     Request::enableHttpMethodParameterOverride();
 
 
-//HOME
+//*************HOME**************
 
     //displays links to brands and stores pages
     $app->get("/", function () use ($app)
@@ -23,101 +23,103 @@
         return $app['twig']->render('index.twig');
     });
 
-//BRANDS
-    //displays ALL brands
+//*************BRAND**************
+
+//READ- displays ALL brands
     $app->get("/brands", function () use ($app)
     {
         return $app['twig']->render('brands.twig');
     });
 
-    //receives ALL info from the form (add a brand) on the brands page
+//CREATE a new brand- receives ALL info from the form (add a brand) on the brands page
     $app->post("/brands", function () use ($app)
     {
         return $app['twig']->render('brands.twig');
     });
 
-    //deletes ALL brands
+//DELETE- deletes ALL brands
     $app->post("/brands", function ($id) use ($app)
     {
         return $app['twig']->render('brands.twig');
     });
 
-    //displays ONE brand and any stores associated with that brand($id)
+//READ- displays ONE brand and any stores associated with that brand($id)
     $app->get("/brand/{id}", function ($id) use ($app)
     {
         return $app['twig']->render('brand.twig');
     });
 
-    //receives info from the form (add a store to the brand) on the ONE brand page
+//CREATE- receives info from the form (add a store to the brand) on the ONE brand page
     $app->post("/brand/{id}", function ($id) use ($app)
     {
         return $app['twig']->render('brand.twig');
     });
 
-    //delete ONE brand by {id}
+//DELETE- delete ONE brand by {id}
     $app->delete("/brand/{id}/delete", function ($id) use ($app)
     {
         return $app['twig']->render('brands.twig');
     });
-    
+
 //NOT NECESSARY FOR THIS ASSIGNMENT
-    //displays brand to be updated or deleted- maybe don't really need GET?  see note with store patch below
+    //READ- displays brand to be updated or deleted- maybe don't really need GET?  see note with store patch below
     // $app->get("/brand/{id}/edit", function ($id) use ($app)
     // {
     //     return $app['twig']->render('/brand_edit.twig');
     // });
 
  
-    //updates the specific brand by id from the brand{id}.
+    //UPDATE- updates the specific brand by id from the brand{id}.
     // $app->patch("/brand/{id}/edit", function ($id) use ($app)
     // {
     //     return $app['twig']->render('brand.twig');
     // });
 
-//STORES
-    //displays ALL stores
+//*************STORES**************
+
+//READ- displays ALL stores
     $app->get("/stores", function () use ($app)
     {
         return $app['twig']->render('stores.twig');
     });
 
-    //receives ALL info from the form (add a store) on the stores page
+//CREATE a new STORE- receives ALL info from the form (add a store) on the stores page
     $app->post("/stores", function () use ($app)
     {
         return $app['twig']->render('stores.twig');
     });
 
-    //deletes ALL stores
+//DELETE- deletes ALL stores
     $app->delete("/stores", function ($id) use ($app)
     {
         return $app['twig']->render('stores.twig');
     });
 
-    //displays ONE store and any brands associated with that store
+//READ- displays ONE store and any brands associated with that store
     $app->get("/store/{id}", function ($id) use ($app)
     {
         return $app['twig']->render('store.twig');
     });
 
-    //receives info from the form (add a brand to the store) on the ONE store page
+//CREATE- receives info from the form (add a brand to the store) on the ONE store page- CAREFUL ON THIS ONE: store to brand or brand to store
     $app->post("/store/{id}", function ($id) use ($app)
     {
         return $app['twig']->render('store.twig');
     });
 
-    //deletes ONE store by {id}
+//DELETE- deletes ONE store by {id}
     $app->delete("/store/{id}/delete", function ($id) use ($app)
     {
         return $app['twig']->render('stores.twig');
     });
 
-    //displays ONE store to be updated or deleted- maybe don't really need GET unless this is where form displays to enter changed info? could also put that on store page with path to /store{id}/edit and never display store_edit.twig?
+//READ- displays ONE store to be updated or deleted- maybe don't really need GET unless this is where form displays to enter changed info? could also put that on store page with path to /store{id}/edit and never display store_edit.twig?
     $app->get("/store/{id}/edit", function ($id) use ($app)
     {
         return $app['twig']->render('store_edit.twig');
     });
 
-    //updates the ONE specific store name using id from store{id}
+//UPDATE- updates the ONE specific store name using id from store{id}
     $app->patch("/store/{id}/edit", function ($id) use ($app)
     {
         return $app['twig']->render('store.twig');;
